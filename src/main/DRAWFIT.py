a92c4b9aec0e571cdf4322ea5d721a452c981a66
diogoDSN <diogofgsnogueira@gmail.com>
2022-04-01 17:34:38 +0100

    Initial sketch of bot's main loop

--- src/main/DRAWFIT.py
+++ src/main/DRAWFIT.py
@@ -3,3 +3,41 @@ from os.path import dirname, abspath
 
 path.append(dirname(abspath(__path__)))
 
+
+import threading
+import asyncio
+
+import discord
+from updates import UpdateHandler
+from commands import CommandHandler
+
+
+
+bot = discord.Client()
+
+def handleCommands():
+    handler = CommandHandler()
+    handler.run()
+
+def handleUpdates():
+    handler = UpdateHandler()
+    handler.run()
+
+
+@bot.event
+async def on_ready():
+
+    # Create thread to handle discord commands
+    commandHandler = threading.Thread(target=handleCommands, name="Thread-1(handleCommands)", daemon=None)
+    commandHandler.start()
+
+    # Start update routine
+    handleUpdates()
+
+def main():
+    TOKEN = ""
+    bot.run(TOKEN)
+
+
+if __name__ == "__main__":
+    main()
