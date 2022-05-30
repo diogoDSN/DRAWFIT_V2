97042f273c832f2190ba25a0c782cc9be09f1210
diogoDSN <diogofgsnogueira@gmail.com>
2022-05-30 16:06:28 +0000

    Improve provided event checks for the bot

--- src/drawfit/bot/utils/utils.py
+++ src/drawfit/bot/utils/utils.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, NoReturn
 
 import discord
 from discord.ext import commands
@@ -12,14 +12,12 @@ def hasPermission(ctx: commands.Context, permission: Permissions) -> bool:
 
 class MessageCheck:
 
-    def __init__(self, guild_name, channel_name, author_name):
-        self.guild = guild_name
-        self.channel = channel_name
-        self.author = author_name
+    def __init__(self, ctx):
+        self.ctx = ctx
 
     def check(self, msg: discord.Message):
         try:
-            return msg.guild.name == self.guild and msg.channel.name == self.channel and str(msg.author) == self.author 
+            return msg.guild == self.ctx.guild and msg.channel == self.ctx.channel and msg.author == self.ctx.author 
         except AttributeError:
             return False
 
@@ -32,3 +30,16 @@ class ReactionAnswerCheck:
         
     def check(self, reaction: discord.Reaction, user: discord.User):
         return user != self.bot and reaction.message in self.messages and str(reaction.emoji) in (Yes(), No())
+
+class ReactionCheck:
+
+    def __init__(self, message: discord.Message, user: discord.User):
+        self.message = message
+        self._user = user
+        
+    @property
+    def user(self) -> discord.User:
+        return self._user
+
+    def check(self, reaction: discord.Reaction, user: discord.User):
+        return user == self.user and reaction.message == self.message
