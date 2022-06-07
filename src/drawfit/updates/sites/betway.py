63677c71a103c4cca6637739a3804eb6805e9e10
diogoDSN <diogofgsnogueira@gmail.com>
2022-06-07 19:39:30 +0000

    Added ' x ' as a possible team separator

--- src/drawfit/updates/sites/betway.py
+++ src/drawfit/updates/sites/betway.py
@@ -20,7 +20,7 @@ class Betway(Site):
     # Types of STOMP Messages to send
 
     def __init__(self) -> NoReturn:
-        super().__init__([' vs '])
+        super().__init__([' vs ', ' x '])
     
     async def getOddsLeague(self, session: AsyncHTMLSession,  league_code: BetwayCode) -> List[OddSample]:
         """
