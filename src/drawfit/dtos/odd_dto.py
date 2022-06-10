a5db47eed507c9f61997ded7c37b750bcea74e65
diogoDSN <diogofgsnogueira@gmail.com>
2022-06-10 02:15:27 +0000

    Added typing and changed constructor to build from domain odd

--- src/drawfit/dtos/odd_dto.py
+++ src/drawfit/dtos/odd_dto.py
@@ -1,15 +1,22 @@
+from __future__ import annotations
+
 from datetime import datetime
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from drawfit.domain.odd import Odd
 
 class OddDto:
 
-    def __init__(self, value: float, date: datetime):
-        self.value = value
-        self.date = date
+    def __init__(self, odd: Odd):
+        self._value: float = odd.value
+        self._date: datetime = odd.date
     
     @property
     def value(self) -> float:
-        return self.value
+        return self._value
 
     @property
     def date(self) -> datetime:
-        return self.date
+        return self._date
