111167d6fbba6d749b7f8dac390ea86b3905ff19
diogoDSN <diogofgsnogueira@gmail.com>
2022-07-19 16:41:13 +0000

    Added timezone to the parameters

--- src/drawfit/utils/time.py
+++ src/drawfit/utils/time.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from pytz import timezone
 
+from drawfit.parameters import TIME_ZONE
+
 def now_lisbon():
-    return datetime.now(timezone('Europe/Lisbon'))
+    return datetime.now(timezone(TIME_ZONE))
