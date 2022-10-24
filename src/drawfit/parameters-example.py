0b0d5c30f42c7ad51971c1adc4c5d7e2fd8d4824
dsn <diogoDSN>
2022-10-24 18:52:10 +0000

    Added queries channel constant to parameters example

--- src/drawfit/parameters-example.py
+++ src/drawfit/parameters-example.py
@@ -6,6 +6,8 @@ SAVE_PATH = "/path/where/to/save" + "drawfit_save.pickle"
 # Commands and updates channels dictionaries. Fill with the servers and their respective channels you want to use.
 COMMAND_CHANNELS = {'your server1' : ['a command channel'], 'your server2' : ['a command channel']}
 UPDATES_CHANNELS = {'your server1' : ['an update channel'], 'your server2' : ['an update channel']}
+QUERIES_CHANNELS = {'your server1' : ['a query channel'  ], 'your server2' : ['a query channel'  ]}
+
 
 
 # Permissions dictionary. Fill the lists with the usernames of the users that have each permission. 
