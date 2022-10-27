1ecad00a172c8823acee589421d88ca0fa8d9e18
diogoDSN <diogofgsnogueira@gmail.com>
2022-10-27 17:37:11 +0100

    Correct database initialisation

--- src/database/initial_database.sql
+++ src/database/initial_database.sql
@@ -1,6 +1,4 @@
-DROP DATABASE drawfit;
-
-DROP TABLE IF EXISTS website cascade;
+DROP TABLE IF EXISTS site cascade;
 DROP TABLE IF EXISTS team cascade;
 DROP TABLE IF EXISTS team_id cascade;
 DROP TABLE IF EXISTS color cascade;
@@ -9,8 +7,6 @@ DROP TABLE IF EXISTS league_code cascade;
 DROP TABLE IF EXISTS plays_in cascade;
 DROP TABLE IF EXISTS game cascade;
 DROP TABLE IF EXISTS odd cascade;
-
-CREATE DATABASE drawfit;
 
 
 ----------------------------------------
@@ -41,7 +37,7 @@ CREATE TABLE team_id(
 
 CREATE TABLE color(
     name        VARCHAR(80)             NOT NULL UNIQUE,
-    hex_code    UNSIGNED MEDIUMINT(255) NOT NULL UNIQUE,
+    hex_code    INTEGER                 NOT NULL UNIQUE,
 
     CONSTRAINT pk_color PRIMARY KEY(name)
 );
@@ -51,18 +47,7 @@ CREATE TABLE league(
     color_name      VARCHAR(80)     NOT NULL,
 
     CONSTRAINT pk_league PRIMARY KEY(name),
-    CONSTRAINT fk_league_color FOREIGN KEY(color) REFERENCES color(name)
-);
-
-CREATE TABLE league_code(
-    league_name         VARCHAR(80)             NOT NULL UNIQUE,
-    site_name           VARCHAR(80)             NOT NULL UNIQUE,
-    code                VARCHAR(80)             NOT NULL,
-
-    CONSTRAINT pk_league_code           PRIMARY KEY(league_name, site_name),
-    CONSTRAINT fk_league_code_league    FOREIGN KEY(team_name) REFERENCES league(name),
-    CONSTRAINT fk_league_code_site      FOREIGN KEY(site_name) REFERENCES site(name),
-    CONSTRAINT u_league_code_site_code  UNIQUE (site_name, code)
+    CONSTRAINT fk_league_color FOREIGN KEY(color_name) REFERENCES color(name)
 );
 
 CREATE TABLE league_code(
@@ -87,7 +72,7 @@ CREATE TABLE plays_in(
 
 CREATE TABLE game(
     name            VARCHAR(80)             NOT NULL,
-    date            SMALLDATETIME           NOT NULL,
+    date            TIMESTAMPTZ             NOT NULL,
     team1_name      VARCHAR(80)             NOT NULL,
     team2_name      VARCHAR(80)             ,
     league_name     VARCHAR(80)             NOT NULL,
@@ -100,10 +85,10 @@ CREATE TABLE game(
 
 CREATE TABLE odd(
     game_name       VARCHAR(80)             NOT NULL,
-    game_date       SMALLDATETIME           NOT NULL,
+    game_date       TIMESTAMPTZ             NOT NULL,
     site_name       VARCHAR(80)             NOT NULL,
-    value           UNSIGNED DECIMAL(4, 2)           NOT NULL,
-    date            SMALLDATETIME           NOT NULL,
+    value           NUMERIC(4, 2)  NOT NULL,
+    date            TIMESTAMPTZ             NOT NULL,
 
     CONSTRAINT pk_odd           PRIMARY KEY(game_name, game_date, site_name),
     CONSTRAINT fk_odd_game      FOREIGN KEY(game_name, game_date) REFERENCES game(name, date),
