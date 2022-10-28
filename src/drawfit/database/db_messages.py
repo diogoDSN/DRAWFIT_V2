8325d0de6355f8079078c02b9affe87a55d94ba4
diogoDSN <diogofgsnogueira@gmail.com>
2022-10-29 00:07:08 +0100

    New database error message

--- src/drawfit/database/db_messages.py
+++ src/drawfit/database/db_messages.py
@@ -1,2 +1,5 @@
 def LeagueNotFound(name: str) -> str:
     return f'The given league: {name}, was not found in the database.'
+
+def TeamNotFound(name: str) -> str:
+    return f'The given team: {name}, was not found in the database.'
