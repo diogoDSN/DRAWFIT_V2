63d89c401a5599fde987ca00a5b3232498029f79
diogoDSN <diogofgsnogueira@gmail.com>
2022-11-22 22:42:46 +0000

    Database store initial version

--- src/drawfit/database/database_store.py
+++ src/drawfit/database/database_store.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import asyncio
 from typing import Dict, List, Tuple, NoReturn, TYPE_CHECKING, Optional
-from psycopg2 import connect, connection
+from psycopg2 import connect
+from psycopg2.errors import UniqueViolation, ForeignKeyViolation, RaiseException
 from datetime import datetime
 
 if TYPE_CHECKING:
@@ -14,7 +15,7 @@ from drawfit.dtos import LeagueDto, DomainDto
 from drawfit.database.drawfit_database_error import DrawfitDatabaseError
 from drawfit.database.db_messages import *
 
-from drawfit.utils import Sites, OddSample, LeagueCode, LeagueCodesFactory
+from drawfit.utils import Sites, OddSample, LeagueCode, LeagueCodesFactory, str_dates
 
 class DatabaseStore:
     
@@ -32,8 +33,11 @@ class DatabaseStore:
         self._db_connection = None
     
     @property
-    def db_connection() -> Optional[connection]:
+    def db_connection():
         return _db_connection
+    
+    def Commit() -> NoReturn:
+        self.db_connection.commit()
 
     def GetAllSites() -> List[str]:
         with self.db_connection.cursor() as cursor:
@@ -54,7 +58,7 @@ class DatabaseStore:
         
         return tuple(color_tuple for color_tuple in stored_colors)
 
-    def GetLeague(self, league_name: str) -> Optional[l.League]:
+    def GetLeague(self, league_name: str) -> l.League:
         with self.db_connection.cursor() as cursor:
             
             # Fetch league and color
@@ -76,8 +80,18 @@ class DatabaseStore:
                     league.codes[site] = LeagueCodesFactory(site, raw_code)
         
         return league
+    
+    def GetTeamNamesFromLeague(self, league_name: str) -> List[str]:
+        with self.db_connection.cursor() as cursor:
             
-    def GetTeam(self, team_name: str) -> Optional[f.Team]:
+            # Fetch team names
+            cursor.execute("SELECT team_name FROM plays_in WHERE league_name=(%s);", (league_name))
+            result = cursor.fetchall()
+            
+        return [team_name for (team_name,) in result]
+            
+            
+    def GetTeam(self, team_name: str) -> f.Team:
         with self.db_connection.cursor() as cursor:
             
             # Fetch team and active status
@@ -96,10 +110,6 @@ class DatabaseStore:
                 site = Sites.SiteFromName(site_name)
                 if not site is None:
                     team.setId(site, team_id)
-            
-            # Fetch team's current game if any
-            cursor.execute("SELECT site_name, id FROM game WHERE team_name = (%s);", (team_name))
-            result = cursor.fetchall()
             
         return team
 
@@ -138,15 +148,122 @@ class DatabaseStore:
                     game.setId(site, game_id)            
             
         return game
-                    
+    
+    
+    def RegisterLeague(league_name: str, league_color: int) -> NoReturn:
+        try:
+            with self.db_connection.cursor() as cursor:
+                cursor.execute("INSERT INTO league VALUES ((%s), (%s))", (league_name, league_color))
+        
+        except UniqueViolation:
+            raise DrawfitDatabaseError(DuplicateLeague(league_name))
+        except ForeignKeyViolation:
+            raise DrawfitDatabaseError(InvalidColor(league_color))
+    
+    def RegisterTeam(team_name: str) -> NoReturn:
+        try:
+            with self.db_connection.cursor() as cursor:
+                cursor.execute("INSERT INTO team VALUES ((%s), true);", (team_name))
+        except UniqueViolation:
+            raise DrawfitDatabaseError(DuplicateTeam(team_name))
+    
+    def RegisterGame(team_name: str, game_name: str, game_date: datetime, league_name: str) -> NoReturn:
+        try:
+            with self.db_connection.cursor() as cursor:
+                cursor.execute("INSERT INTO game VALUES ((%s), (%s), (%s), (%s));", (team_name, game_name, game_date, league_name))
                 
+        except UniqueViolation as e:
+            if team_name in e.pgerror:
+                raise DrawfitDatabaseError(TeamAlreadyHasGame(team_name, str_dates(game_date)))
+            else:
+                raise DrawfitDatabaseError(GameAlreadyRegistered(game_name, str_dates(game_date)))
             
+        except ForeignKeyViolation as e:
+            if team_name in e.pgerror and league_name in e.pgerror:
+                raise DrawfitDatabaseError(TeamNotInLeague(team_name, league_name))
+            elif team_name in e.pgerror:
+                raise DrawfitDatabaseError(TeamNotFound(team_name))
+            else:
+                raise DrawfitDatabaseError(LeagueNotFound(league_name))
+        
+        except RaiseException as e:
+            raise DrawfitDatabaseError(e.pgerror)
+    
+    def RegisterOdd(game_name: str, game_date: datetime, site: Site, value: float, date: datetime):
+        try:
+            with self.db_connection.cursor() as cursor:
+                cursor.execute("INSERT INTO odd VALUES ((%s), (%s), (%s), (%s), (%s));", \
+                    (game_name, game_date, site.value, value, date))
+                
+        except UniqueViolation:
+            raise DrawfitDatabaseError(DuplicateOdd(game_name, site.value, str_dates(date)))
+        
+        except ForeignKeyViolation:
+            if game_name in e.pgerror:
+                raise DrawfitDatabaseError(GameNotFound(game_name, str_dates(game_date)))
+            else:
+                raise DrawfitDatabaseError(SiteNotFound(site.value))
+        
+        except RaiseException as e:
+            raise DrawfitDatabaseError(e.pgerror)
+    
+    def AddTeamToLeague(team_name: str, league_name: str) -> NoReturn:
+        try:
+            with self.db_connection.cursor() as cursor:
+                cursor.execute("INSERT INTO plays_in VALUES ((%s), (%s));", (team_name, league_name))
+        
+        except UniqueViolation:
+            pass
+        
+        except ForeignKeyViolation as e:
+            if team_name in e.pgerror:
+                raise DrawfitDatabaseError(TeamNotFound(team_name))
+            else:
+                raise DrawfitDatabaseError(LeagueNotFound(league_name))
+    
+    def AddLeagueCode(league_name: str, site: Site, code: str) -> NoReturn:
+        try:
+            with self.db_connection.cursor() as cursor:
+                cursor.execute("INSERT INTO league_code VALUES ((%s), (%s), (%s));", \
+                        (league_name, site.value, code))
+        
+        except UniqueViolation as e:
+            if league_name in e.pgerror:
+                raise DrawfitDatabaseError(DuplicateLeagueCode(league_name, site.value))
+            else:
+                raise DrawfitDatabaseError(RepeatedSiteCode(site.value, code))
+        
+        except ForeignKeyViolation as e:
+            if league_name in e.pgerror:
+                raise DrawfitDatabaseError(LeagueNotFound(league_name))
+            else:
+                raise DrawfitDatabaseError(SiteNotFound(site.value))
+    
+    def AddTeamId(team_name: str, site: Site, id: str) -> NoReturn:
+        try:
+            with self.db_connection.cursor as cursor:
+                cursor.execute("INSERT INTO team_id VALUES ((%s), (%s), (%s));", (team_name, site.value, id))
+        
+        except UniqueViolation:
+            raise DrawfitDatabaseError(DuplicateTeamId(team_name, site.value))
+        
+        except ForeignKeyViolation as e:
+            if team_name in e.pgerror:
+                raise DrawfitDatabaseError(TeamNotFound(team_name))
+            else:
+                raise DrawfitDatabaseError(SiteNotFound(site.value))
+    
+    def AddGameId(game_name: str, game_date: datetime, site: Site, id: str) -> NoReturn:
+        try:
+            with self.db_connection.cursor() as cursor:
+                cursor.execute("INSERT INTO game_id VALUES ((%s), (%s), (%s), (%s));",\
+                        (game_name, game_date, site.value, id))
             
-            
-            
-            
-            
-            
-            
-            
-            
+        except UniqueViolation:
+            raise DrawfitDatabaseError(DuplicateGameId(game_name, str_dates(game_date), site.value))
+        
+        except ForeignKeyViolation as e:
+            if game_name in e.pgerror:
+                raise DrawfitDatabaseError(GameNotFound(game_name, str_dates(game_date)))
+            else:
+                raise DrawfitDatabaseError(SiteNotFound(site.value))
--- src/drawfit/database/db_messages.py
+++ src/drawfit/database/db_messages.py
@@ -1,5 +1,44 @@
+def SiteNotFound(name: str) -> str:
+    return f'The given site: [{name}], was not found in the database.'
+
 def LeagueNotFound(name: str) -> str:
-    return f'The given league: {name}, was not found in the database.'
+    return f'The given league: [{name}], was not found in the database.'
 
 def TeamNotFound(name: str) -> str:
-    return f'The given team: {name}, was not found in the database.'
+    return f'The given team: [{name}], was not found in the database.'
+
+def GameNotFound(name: str, date: str) -> str:
+    return f'The given game: [{name}] at date [{date}], was not found in the database.'
+
+def DuplicateLeague(name: str) -> str:
+    return f'Tried to register a duplicate league name: [{name}].'
+
+def DuplicateTeam(name: str) -> str:
+    return f'Tried to register a duplicate team name: [{name}].'
+
+def InvalidColor(value: int) -> str:
+    return f'The given color value: [{value}], is invalid.'
+
+def DuplicateOdd(game_name: str, site_name: str, odd_date: str) -> str:
+    return f'The game [{game_name}] already has an odd registered for [{site_name}] at [{odd_date}].'
+
+def TeamAlreadyHasGame(team_name: str, game_date: str) -> str:
+    return f'The team [{team_name}] already has a game on [{game_date}].'
+
+def GameAlreadyRegistered(game_name: str, game_date: str) -> str:
+    return f'There is a game with the name [{game_name}] already registered for date [{game_date}].'
+
+def TeamNotInLeague(team_name: str, league_name: str) -> str:
+    return f'The team [{team_name}] doesn\'t play in the league [{league_name}].'
+
+def DuplicateLeagueCode(league_name: str, site_name: str) -> str:
+    return f'The league [{league_name}] already has a code registered for the site [{site_name}].'
+
+def RepeatedSiteCode(site_name: str, code: str) -> str:
+    return f'The site [{site_name}] already has a the code [{code}] registered.'
+
+def DuplicateTeamId(team_name: str, site_name: str) -> str:
+    return f'The team [{team_name}] already has an id registered for the site [{site_name}].'
+
+def DuplicateGameId(game_name: str, game_date: str, site_name: str) -> str:
+    return f'The game [{game_name}] at date [{game_date}] already has an id registered for the site [{site_name}].'
