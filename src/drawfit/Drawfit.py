1af5672cefc8fa0544e42c959ff5066ab244c064
diogoDSN <diogofgsnogueira@gmail.com>
2022-12-26 23:11:45 +0000

    Added logger command line args

--- src/drawfit/Drawfit.py
+++ src/drawfit/Drawfit.py
@@ -1,3 +1,4 @@
+import argparse
 import sys
 from os.path import dirname, abspath
 
@@ -7,9 +8,22 @@ sys.path.append(dirname(abspath(__file__)))
 import drawfit.bot.drawfit_bot as dbot
 from drawfit.parameters import TOKEN_PATH
 from discord.errors import LoginFailure
+from drawfit.utils import DEBUG_MODE, SHELL_MODE
 
 def main():
 
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('-s', '--shell', action='store_true')
+    
+    args = parser.parse_args()
+    
+    DEBUG_MODE = args.debug
+    SHELL_MODE = args.shell
+        
+    
+    # Run bot
     with open(TOKEN_PATH, 'r') as f:
         TOKEN = f.readline()[:-1]
 
