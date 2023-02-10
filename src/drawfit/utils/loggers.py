f89014cc20206fdbc45dbc971c2c16b93a6b9708
dsn <diogoDSN>
2023-02-10 14:31:20 +0000

    Multiple parameters changed to environment variables

--- src/drawfit/utils/loggers.py
+++ src/drawfit/utils/loggers.py
@@ -1,11 +1,12 @@
 import logging
+from os import environ
 
 import drawfit.parameters as prm
 
 def create_new_logger(name: str):
     logger = logging.getLogger(name)
-    
-    handler = logging.FileHandler(f'{prm.LOGS_PATH}/{name}.log')
+
+    handler = logging.FileHandler('{}/{}.log'.format(environ['LOGS_PATH'], name))
     handler.setFormatter(logging.Formatter(fmt='[%(asctime)s][%(levelname)s] - %(message)s', datefmt='%y/%m/%d %H:%M:%S'))
     logger.addHandler(handler)
     
